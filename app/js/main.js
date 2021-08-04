ebd8a5012c3d1f4b293580471445945dafc51e23
Lance Martija <jacobmartija03@gmail.com>
2021-08-05 00:10:11 +0800

    feat: add event listener for burger menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,8 @@
-console.log('hello world!');
+const burgerBtn = document.querySelector('#burger-menu')
+
+burgerBtn.addEventListener('click', () => {
+  if (burgerBtn.classList.contains('active'))
+    burgerBtn.classList.remove('active')
+  else
+    burgerBtn.classList.add('active')
+})
