1b31e78e92363e95fea177d35deb93ec3c6d99f5
Lance Martija <jacobmartija03@gmail.com>
2021-08-05 17:24:36 +0800

    fix: fix burger button functionality

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,28 @@
-const burgerBtn = document.querySelector('#burger-menu')
+const burgerBtn = document.querySelector('#burger-button')
+const burgerNav = document.querySelector('#burger-nav')
+const aboutAnchorTag = document.querySelector('#about');
+const serviceAnchorTag = document.querySelector('#service');
+let burgerBtnIsActive = false;
+
+const removeBurgerNav = () => {
+  burgerBtn.classList.remove('active')
+  burgerNav.classList.remove('toggle-visibility')
+  burgerNav.classList.remove('fade-in')
+  burgerNav.classList.add('fade-out')
+  burgerBtnIsActive = false;
+}
+
+const addBurgerNav = () => {
+  burgerBtn.classList.add('active')
+  burgerNav.classList.add('toggle-visibility')
+  burgerNav.classList.add('fade-in')
+  burgerNav.classList.remove('fade-out')
+  burgerBtnIsActive = true;
+}
 
 burgerBtn.addEventListener('click', () => {
-  if (burgerBtn.classList.contains('active'))
-    burgerBtn.classList.remove('active')
-  else
-    burgerBtn.classList.add('active')
+  if (!burgerBtnIsActive) addBurgerNav()
+  else removeBurgerNav()
+  aboutAnchorTag.addEventListener('click', removeBurgerNav)
+  serviceAnchorTag.addEventListener('click', removeBurgerNav)
 })
